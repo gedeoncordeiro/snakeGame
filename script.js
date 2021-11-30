1ea74ef6459c539646eafeaf4cb50f2b2d20a016
Gedeon Cordeiro <gedeon.donn@gmail.com>
2021-11-29 22:51:03 -0300

    Finalizando Jogo para ajustes finais

--- script.js
+++ script.js
@@ -44,11 +44,18 @@ function update (event){
 }
 
 function iniciarJogo(){
+   
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
+    for(i=1 ; i < snake.length; i++){
+        if(snake[0].x == snake[1].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over :(')
+        }
+    }
 
     criarBG();
     criarCobrinha(); 
@@ -62,7 +69,15 @@ function iniciarJogo(){
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
-    snake.pop(); /* adicionando aqui a função pop, que retira o último elemento do array*/
+
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    }
+    else{food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
+
+    // snake.pop(); /* adicionando aqui a função pop, que retira o último elemento do array*/
     
     let newHead = {
         x: snakeX,
